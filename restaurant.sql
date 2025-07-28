d2169fecfc83155e529078faf1dfaa22e1bbc4f2
Safat Uddin <122455247+SafatUddin@users.noreply.github.com>
2025-07-28 11:26:44 +0600

    Update restaurant.sql

--- restaurant.sql
+++ restaurant.sql
@@ -74,10 +74,6 @@ CREATE TABLE `menuitem` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `menuitem`
---
-
-INSERT INTO `menuitem` (`itemId`, `itemName`, `catName`, `price`, `status`, `description`, `image`, `dateCreated`, `updatedDate`, `is_popular`)
 
 -- --------------------------------------------------------
 
